61053ffbc2f662953bf02f1d73ad07a53c8b46b4
Conary <31629328+conaryHernandez@users.noreply.github.com>
2020-03-30 15:54:22 -0600

    cache expiration

--- NodeAdvanced/services/cache.js
+++ NodeAdvanced/services/cache.js
@@ -7,7 +7,17 @@ const client = redis.createClient(redisUrl);
 client.get = util.promisify(client.get);
 const exec = mongoose.Query.prototype.exec;
 
+mongoose.Query.prototype.cache = function() {
+  this.useCache = true;
+
+  return this;
+};
+
 mongoose.Query.prototype.exec = async function() {
+  if (!this.useCache) {
+    return exec.apply(this, arguments);
+  }
+
   const key = JSON.stringify(
     Object.assign({}, this.getQuery(), {
       collection: this.mongooseCollection.name
@@ -29,7 +39,7 @@ mongoose.Query.prototype.exec = async function() {
   // otherwise, issue the query and store the result in redis
   const result = await exec.apply(this, arguments);
 
-  client.set(key, JSON.stringify(result));
+  client.set(key, JSON.stringify(result), 'EX', 10);
 
   return result;
 };
