e3bd6b414104abd8a9b37aa043b332b746c4ba70
Conary <31629328+conaryHernandez@users.noreply.github.com>
2020-04-04 10:39:38 -0600

    blogs testing

--- NodeAdvanced/tests/header.test.js
+++ NodeAdvanced/tests/header.test.js
@@ -13,7 +13,7 @@ afterEach(async () => {
 });
 
 test('The Header has the correct text', async () => {
-  const text = await page.$eval('a.brand-logo', (el) => el.innerHTML);
+  const text = await page.getContentsOf('a.brand-logo');
 
   expect(text).toEqual('Blogster');
 });
@@ -29,7 +29,7 @@ test('clicking login start oauth flow', async () => {
 test('When signed in, show logout button', async () => {
   await page.login();
 
-  const text = await page.$eval('a[href="/auth/logout"]', (el) => el.innerHTML);
+  const text = await page.getContentsOf('a[href="/auth/logout"]');
 
   expect(text).toEqual('Logout');
 });
