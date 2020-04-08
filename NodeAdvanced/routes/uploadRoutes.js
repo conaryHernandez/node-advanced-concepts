f3fd275bb8416ccfa2b412c6efb369cf6e3b7911
Conary <31629328+conaryHernandez@users.noreply.github.com>
2020-04-08 17:06:55 -0600

    aws upload config

---
+++ NodeAdvanced/routes/uploadRoutes.js
@@ -0,0 +1,25 @@
+const AWS = require('aws-sdk');
+const uuid = require('uuid');
+const keys = require('../config/keys');
+const requireLogin = require('../middlewares/requireLogin');
+
+const s3 = new AWS.S3({
+  accessKeyId: keys.accessKeyId,
+  secretAccessKey: keys.secretAccessKey,
+});
+
+module.exports = (app) => {
+  app.get('/api/upload', requireLogin, (req, res) => {
+    const key = `${req.user._id}/${uuid()}.jpeg`;
+
+    s3.getSignedUrl(
+      'putObject',
+      {
+        Bucket: 'my-blog-bucket-hn',
+        ContentType: 'image/jpeg',
+        Key: key,
+      },
+      (err, url) => res.send({ url, key })
+    );
+  });
+};
